9123206f9fd8329a5f0043973f3586eb9d5d0b5e
Priyanka Pansuriya <99384120+Priyankapansuriya@users.noreply.github.com>
2022-12-19 18:22:45 +0530

    First Commit

--- Src/Screens/Setting/ChangeUserProfile.js
+++ Src/Screens/Setting/ChangeUserProfile.js
@@ -23,39 +23,44 @@ import l1 from '../../../Assets/l1.png';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {firebase} from '../../Firebase/Config';
 import {launchImageLibrary} from 'react-native-image-picker';
-import ImagePicker from 'react-native-image-picker';
+import ImagePicker, {openPicker} from 'react-native-image-crop-picker';
 
 const ChangeUserProfile = ({navigation}) => {
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const takephoto = () => {
-    ImagePicker.openCamera({
+  const pickImage = async () => {
+    let result = await ImagePicker.openPicker({
       width: 300,
       height: 400,
       cropping: true,
-    }).then(image => {
-      console.log(image);
-      setImage(image, path);
-      this.bs.current.handleUpload();
+      aspect: [1, 1],
     });
-  };
-  const choosefromlibrary = () => {
-    ImagePicker.openPicker({
-      width: 300,
-      height: 400,
-      cropping: true,
-    }).then(image => {
-      console.log(image);
-      setImage(image, path);
-      this.bs.current.handleUpload();
-    });
+    console.log(result);
+    if (!result.cancelled) {
+      const source = {uri: result.uri};
+      setImage(source);
+
+      const response = await fetch(result.uri);
+      const blob = await response.blob();
+      const filename = result.uri.substring(result.uri);
+
+      const ref = firebase.storage().ref().child(filename);
+      const snapshot = await ref.put(blob);
+      const url = await snapshot.ref.getDownloadURL();
+
+      console.log(url);
+      return url;
+    } else {
+      return null;
+    }
   };
   const handleUpload = () => {
+    pickImage();
     AsyncStorage.getItem('user').then(data => {
       setLoading(true);
 
-      takephoto().then(url => {
+      pickImage().then(url => {
         fetch('http://192.168.43.155:3000/setprofilepic', {
           method: 'post',
           headers: {
@@ -71,7 +76,7 @@ const ChangeUserProfile = ({navigation}) => {
             if (data.message === 'Profile picture updated successfully') {
               setLoading(false);
               alert('Profile picture updated successfully');
-              navigation.navigate('Settings_1');
+              navigation.navigate('Setting1');
             } else if (data.error === 'Invalid Credentials') {
               alert('Invalid Credentials');
               setLoading(false);
@@ -83,41 +88,14 @@ const ChangeUserProfile = ({navigation}) => {
           })
           .catch(err => {
             console.log(err);
-          });
-      });
-      choosefromlibrary().then(url => {
-        fetch('http://192.168.43.155:3000/setprofilepic', {
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email: JSON.parse(data).user.email,
-            profilepic: url,
-          }),
-        })
-          .then(res => res.json())
-          .then(data => {
-            if (data.message === 'Profile picture updated successfully') {
-              setLoading(false);
-              alert('Profile picture updated successfully');
-              navigation.navigate('Settings_1');
-            } else if (data.error === 'Invalid Credentials') {
-              alert('Invalid Credentials');
-              setLoading(false);
-              navigation.navigate('Login');
-            } else {
-              setLoading(false);
-              alert('Please Try Again');
-            }
           })
           .catch(err => {
             console.log(err);
+            throw err;
           });
       });
     });
   };
-
   return (
     <View style={containerFull}>
       <TouchableOpacity
@@ -136,13 +114,13 @@ const ChangeUserProfile = ({navigation}) => {
       <Image source={l1} style={logo1} />
       <Text style={formHead2}>Choose a profile Image</Text>
 
-      {loading ? (
+      {/* {loading ? (
         <ActivityIndicator size="large" color="maroon" />
       ) : (
         <Text style={formbtn} onPress={() => handleUpload()}>
           Take Photo
         </Text>
-      )}
+      )} */}
       {loading ? (
         <ActivityIndicator size="large" color="maroon" />
       ) : (
