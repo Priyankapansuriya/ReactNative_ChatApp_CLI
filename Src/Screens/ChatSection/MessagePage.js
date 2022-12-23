517138f8590cc3c0864bd237a8027990615bc22a
Priyanka Pansuriya <99384120+Priyankapansuriya@users.noreply.github.com>
2022-12-23 18:44:33 +0530

    Complete

--- Src/Screens/ChatSection/MessagePage.js
+++ Src/Screens/ChatSection/MessagePage.js
@@ -10,24 +10,44 @@ import {
   TouchableOpacity,
   TextInput,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import noprofile from '../../../Assets/noprofile.png';
 import Icon from 'react-native-vector-icons/Ionicons';
+import io from 'socket.io-client';
+
+const socket = io('http://192.168.43.155:3001');
 
 const MessagePage = ({navigation, route}) => {
-  const {fuseremail} = route.params;
+  const {fuseremail, fuserid} = route.params;
   const [ouruserdata, setOuruserdata] = useState(null);
   const [fuserdata, setFuserdata] = useState(null);
+  const [userid, setUserid] = useState(null);
+  const [roomid, setRoomid] = useState(null);
+  const [chat, setChat] = useState(['']);
+
   useEffect(() => {
     loaddata();
     //console.log(fuseremail);
   }, []);
 
+  useEffect(() => {
+    socket.on('receive_message', data => {
+      console.log('recieved message - ', data);
+      loadMessages(roomid);
+    });
+  }, [socket]);
+
+  const sortroomid = (id1, id2) => {
+    if (id1 > id2) {
+      return id1 + id2;
+    } else {
+      return id2 + id1;
+    }
+  };
+
   const loaddata = async () => {
     AsyncStorage.getItem('user')
       .then(async value => {
-        //console.log('async userdata', data);
-        //setOuruserdata(JSON.stringify(data));
         fetch('http://192.168.43.155:3000/userdata', {
           method: 'POST',
           headers: {
@@ -41,6 +61,7 @@ const MessagePage = ({navigation, route}) => {
             if (data.message == 'User Found') {
               console.log('our user data', data.user.username);
               setOuruserdata(data.user);
+              setUserid(data.user._id);
 
               fetch('http://192.168.43.155:3000/otheruserdata', {
                 method: 'POST',
@@ -50,16 +71,19 @@ const MessagePage = ({navigation, route}) => {
                 body: JSON.stringify({email: fuseremail}),
               })
                 .then(res => res.json())
-                .then(data => {
-                  if (data.message == 'User Found') {
-                    //console.log('fuser data ', data.user.username);
-                    setFuserdata(data.user);
-                    ismyprofile(data.user);
-                    CheckFollow(data.user);
+                .then(async data1 => {
+                  if (data1.message == 'User Found') {
+                    console.log('fuser data ', data1.user.username);
+                    setFuserdata(data1.user);
+                    let temproomid = await sortroomid(fuserid, data.user._id);
+
+                    setRoomid(temproomid);
+                    // console.log('room id ', temproomid)
+                    socket.emit('join_room', {roomid: temproomid});
+                    loadMessages(temproomid);
                   } else {
                     alert('User Not Found');
                     navigation.navigate('SearchUserPage');
-                    // navigation.navigate('Login')
                   }
                 })
                 .catch(err => {
@@ -81,6 +105,60 @@ const MessagePage = ({navigation, route}) => {
       });
   };
 
+  const [currentmessage, setCurrentmessage] = useState(null);
+
+  const sendMessage = async () => {
+    const messagedata = {
+      message: currentmessage,
+      roomid: roomid,
+      senderid: userid,
+      recieverid: fuserdata._id,
+    };
+    fetch('http://192.168.43.155:3000/savemessagetodb', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(messagedata),
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.message == 'Message saved successfully') {
+          socket.emit('send_message', messagedata);
+          loadMessages(roomid);
+          console.log('message sent');
+
+          setCurrentmessage('');
+        } else {
+          alert('Network Error');
+          setCurrentmessage('');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    loadMessages(roomid);
+  }, [chat]);
+
+  const loadMessages = temproomid => {
+    fetch('http://192.168.43.155:3000/getmessages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({roomid: temproomid}),
+    })
+      .then(res => res.json())
+      .then(data => {
+        setChat(data);
+      });
+  };
+
+  const scrollViewRef = useRef();
+
   return (
     <View style={styles.container}>
       <View style={styles.s1}>
@@ -99,14 +177,50 @@ const MessagePage = ({navigation, route}) => {
         )}
         <Text style={styles.username}>{fuserdata?.username}</Text>
       </View>
+
+      <ScrollView
+        style={styles.messageView}
+        ref={scrollViewRef}
+        onContentSizeChange={() =>
+          scrollViewRef.current.scrollToEnd({animated: true})
+        }>
+        {chat.map((item, index) => {
+          return (
+            <View style={styles.message} key={index}>
+              {item.senderid == userid && (
+                <View style={styles.messageRight}>
+                  <Text style={styles.messageTextRight}>{item.message}</Text>
+                </View>
+              )}
+              {item.senderid != userid && item != '' && (
+                <View style={styles.messageLeft}>
+                  <Text style={styles.messageTextLeft}>{item.message}</Text>
+                </View>
+              )}
+            </View>
+          );
+        })}
+      </ScrollView>
+
       <View style={styles.sbottom}>
         <TextInput
           style={styles.sbottominput}
           placeholder="Type a message"
           placeholderTextColor={'grey'}
+          onChangeText={text => setCurrentmessage(text)}
+          value={currentmessage}
         />
-        <TouchableOpacity style={styles.sbottombtn}>
-          <Icon name="ios-send-outline" size={24} color="white" />
+        <TouchableOpacity>
+          {currentmessage ? (
+            <Icon
+              name="ios-send-outline"
+              size={24}
+              color="white"
+              onPress={() => sendMessage()}
+            />
+          ) : (
+            <Icon name="ios-send-outline" size={24} color="white" />
+          )}
         </TouchableOpacity>
       </View>
     </View>
